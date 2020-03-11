00ec7e6d6e9c07dff45e6a738d09c5811c3889b1
hun <huod28@gmail.com>
2020-03-11 16:55:03 +0900

    메일 폼 경로 수정

--- src/main/java/com/wooriat/admin/common/utility/MailUtil.java
+++ src/main/java/com/wooriat/admin/common/utility/MailUtil.java
@@ -1,16 +1,21 @@
 package com.wooriat.admin.common.utility;
 
+import java.io.File;
 import java.io.StringWriter;
 import java.util.Properties;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletContext;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
 import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
@@ -27,7 +32,15 @@ public class MailUtil {
         MimeMessage message = javaMailSender.createMimeMessage();
 
         if(templateYn != null && templateYn.equals("Y")){
-            Template template = velocityEngine.getTemplate("/src/main/webapp/templates/"+templateFileName+".vm", "UTF-8");
+
+            Properties p = new Properties();
+            p.setProperty("resource.loader", "class");
+            p.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+            Velocity.init( p );
+
+            Template template = Velocity.getTemplate("templates/"+templateFileName+".vm", "UTF-8");
+
+            //Template template = velocityEngine.getTemplate("templates/"+templateFileName+".vm", "UTF-8");
 
             StringWriter stringWriter = new StringWriter();
             template.merge(velocityContext, stringWriter);
